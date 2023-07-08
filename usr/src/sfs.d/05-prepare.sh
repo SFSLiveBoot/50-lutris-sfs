3823fb4f7ef93b48593631b16899eac58a222c35
lauri <lauri@korc.jp>
2023-07-08 14:05:05 +0900

    add extra pkgs option

--- usr/src/sfs.d/05-prepare.sh
+++ usr/src/sfs.d/05-prepare.sh
@@ -4,3 +4,4 @@ mkdir -p /usr/src/deb
 cp /var/lib/dpkg/status /usr/src/deb/dpkg_status.pre-build
 touch /usr/src/deb/Packages
 echo "deb [trusted=yes] file:///usr/src/deb ./" >/etc/apt/sources.list.d/lutris.list
+dpkg --add-architecture i386
--- usr/src/sfs.d/20-install.sh
+++ usr/src/sfs.d/20-install.sh
@@ -4,4 +4,4 @@ set -e
 
 . "$(dirname "$0")/.common.sh"
 
-dpkg_status=/usr/src/deb/dpkg_status.pre-build "$lbu/scripts/apt-sfs.sh" "$DESTDIR" lutris
+dpkg_status=/usr/src/deb/dpkg_status.pre-build "$lbu/scripts/apt-sfs.sh" "$DESTDIR" lutris ${extra_pkgs:+$extra_pkgs}
